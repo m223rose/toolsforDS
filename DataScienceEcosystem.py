bfd25c22806db0c0ca185f6d86f167fa4f37ac3a
m223rose <58744695+m223rose@users.noreply.github.com>
2023-09-17 20:20:58 -0700

    upload week 6

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Exercise 2: Create markdown cell with title of the notebook
+# 
+# # Data Science Tools and Ecosystem
+
+# Exercise 3 - Create a markdown cell for an introduction
+# 
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Exercise 10 - Insert a markdown cell to list Objectives
+# 
+# **Objectives:**
+# - List popular languages for Data Science
+# - List commonly used libraries for Data Science
+# - Show examples of evaluating arithmetic expressions in Python
+# 
+
+# Exercise 4 - Create a markdown cell to list data science language
+# 
+# 1. Python
+# 2. R
+# 3. Julia
+# 
+
+# Exercise 5 - Create a markdown cell to list data science libraries
+# 
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. SciPy
+# 3. Matplotlib
+# 
+# 
+
+# Exercise 6 - Create a markdown cell with a table of Data Science tools
+# 
+# |Data Science Tools|
+# ----
+# |Git|
+# |Jupyter notebooks|
+# |R Studio|
+# 
+
+# Exercise 7 - Create a markdown cell introducing arithmetic expression examples
+# 
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[2]:
+
+
+## Exercise 8 - Create a code cell to multiply and add numbers
+
+## This is a simple arithmetic expression to multiply then add integers
+
+(3*4)+5
+
+
+# In[3]:
+
+
+## Exercise 9 - Create a code cell to convert minutes to hours
+
+## This will convert 200 minutes to hours by diving by 60.
+
+200/60
+
+
+# Exercise 11 - Create a markdown cell to indicate the Author's name
+# 
+# ## Author
+# Molly 
+# 
